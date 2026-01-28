914dfd0ad068ab63d4b45c0b2c77f8aafb7f5d6b
MohamedELBERMIL69780 <mohamed-du-69780@hotmail.fr>
2026-01-28 12:34:36 +0100

    update

--- serenia/app/(tabs)/_layout.tsx
+++ serenia/app/(tabs)/_layout.tsx
@@ -5,6 +5,7 @@ import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -26,15 +27,32 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          href: null,
         }}
       />
       <Tabs.Screen
         name="patient"
         options={{
-          title: 'Patient',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
+          title: 'Bot',
+          tabBarIcon: ({ color }) => <Ionicons size={26} name="chatbubble-ellipses-outline" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="help"
+        options={{
+          href: null,
         }}
       />
     </Tabs>
